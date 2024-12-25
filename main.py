b285b5429a30f9a7f5bde2cab463f18b949d0c5f
Вадим Голов <grafit-design@inbox.ru>
2024-12-25 17:34:52 +0500

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+def count_vowels(some_text: str) -> int:
+
+    vowels: str = 'аеёиоуыэюяaeiouy'
+
+    vowel_count: int = 0
+
+    for char in some_text.lower():
+        if char in vowels:
+            vowel_count += 1
+
+    return vowel_count
+
+
+# Пример использования
+user_text: str = input('Введите слово, словосочетание или предложение: ')
+vowels_amount: int = count_vowels(user_text)
+print(f'Количество гласных букв в тексте: {vowels_amount}')
