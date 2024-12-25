f181dfc46647a21d2bfdeec79cb72ee42c7ccf35
Вадим Голов <grafit-design@inbox.ru>
2024-12-25 23:39:24 +0500

    Все готово

--- test_file.py
+++ test_file.py
@@ -4,7 +4,7 @@ from main import count_vowels
 
 def test1_count_vowels():
     assert count_vowels('hello') == 2
-    assert count_vowels('world') == 1
+    assert count_vowels('world') == 2
     assert count_vowels('свобода') == 3
 
 
@@ -19,9 +19,8 @@ def test1_count_vowels():
     ('Bird', 1),
     ('Река', 2),
     ('Computer', 3),
-    ('Шмрц', 0)
+    ('Шмрц', 0),  # Чешский мотогонщик Якуб Шмрц
+    ('Ааа Oоо You', 9)  # Смесь русских и латинских символов
 ])
 def test2_count_vowels(text, expected):
     assert count_vowels(text) == expected
-
-# Чешский мотогонщик Якуб Шмрц
